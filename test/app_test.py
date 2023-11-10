ad54a7a50e08aa2089b96e34827c5aa2989662d7
Nikolay Kostandiev <94651604+nnniki@users.noreply.github.com>
2023-11-10 12:56:26 +0200

    Update app_test.py

--- test/app_test.py
+++ test/app_test.py
@@ -1,7 +1,6 @@
 import unittest
 
-from app import app
-
+from src.app import app
 
 class TestApp(unittest.TestCase):
     def setUp(self):
@@ -10,7 +9,6 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-
 
 if __name__ == "__main__":
     unittest.main()
