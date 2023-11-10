87d1668f9e754bbcde70344449813948d293ba7c
Nikolay Kostandiev <94651604+nnniki@users.noreply.github.com>
2023-11-10 10:51:30 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello! I am a Flask application running on {}".format(socket.gethostname())
+    name = socket.gethostname()
+    return "Hello! I am a Flask application running on {}".format(name)
 
 
 if __name__ == '__main__':
